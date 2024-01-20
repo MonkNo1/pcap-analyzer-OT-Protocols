55a3cf1ecb10e207ff46196412062cb79d01154d
Monk <61575072+MonkNo1@users.noreply.github.com>
2024-01-20 08:30:22 +0530

    get user inputs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pyshark 
 import csv
+import pickle
+
+
 
 pcapfile = "130423-1.pcapng"
 header = ["timestamp","source","Destination","ANSI","DATA"]
@@ -18,22 +21,26 @@ def outcsv(pktcsv):
        
        
 if __name__=="__main__":   
-    capture = pyshark.FileCapture('130423-1.pcapng',display_filter='cip')
-    for pkt in capture:
-        cal = []
-        try : 
-            cal.append(pkt.sniff_timestamp)
-            cal.append(pkt.ip.src)
-            cal.append(pkt.ip.dst)
-            try:
-                cal.append(pkt.cip.symbol)
-            except:
-                cal.append(" ")
+    dfil = input("Enter the protocol to appaly display Filter : ")
+    try :
+        capture = pyshark.FileCapture('130423-1.pcapng',display_filter=dfil)
+        for pkt in capture:
+            cal = []
             try : 
-                cal.append(pkt.cipcls.get_field_by_showname('Data'))
-            except:
-                cal.append(" ")
-            temp.append(cal)
-        except :
-            pass
-    outcsv(temp)
+                cal.append(pkt.sniff_timestamp)
+                cal.append(pkt.ip.src)
+                cal.append(pkt.ip.dst)
+                try:
+                    cal.append(pkt.cip.symbol)
+                except:
+                    cal.append(" ")
+                try : 
+                    cal.append(pkt.cipcls.get_field_by_showname('Data'))
+                except:
+                    cal.append(" ")
+                temp.append(cal)
+            except :
+                pass
+        outcsv(temp)
+    except : 
+        print("Enter the correct value !!!")
